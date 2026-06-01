4ad3c2ef85ddc2fd9b46b8b8a3ddfe034bf8f551
bmyvrtkhpj-lang <naveensharmacivil8@gmail.com>
2026-06-01 13:37:14 +0530

    Remove unused import statement
    Removed unused import for plotly.graph_objects.

--- capm_dashboard_reconstructed.py
+++ capm_dashboard_reconstructed.py
@@ -18,7 +18,6 @@ from datetime import datetime, timedelta
 
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 import streamlit as st
 import yfinance as yf
 from mftool import Mftool
